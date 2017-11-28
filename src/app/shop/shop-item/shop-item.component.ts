1b70da3551f1cff1eef64e7558a9395143e7e87b
Robert Cazacu <taigi100@gmail.com>
2017-11-28 06:58:08 +0200

    Remove price from shopping card

--- src/app/shop/shop-item/shop-item.component.ts
+++ src/app/shop/shop-item/shop-item.component.ts
@@ -2,24 +2,22 @@ import { ShopItem } from './../../domain/shop-item.model';
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-shop-item',
-  template: `
+   selector: 'app-shop-item',
+   template: `
     <div class="item" *ngIf="item">
       <div class="card" style="width: 20rem;">
       <img class="card-img-top" [src]="item.image" alt="Item image not found">
       <div class="card-block">
         <h4 class="card-title">{{item.name}}</h4>
-        <p class="card-text">{{item.price}}
-          <a [href]="item.linkToBuy" class="btn btn-primary buy-button">Buy now!</a> 
-        </p>
+          <a [href]="item.linkToBuy" class="btn btn-primary buy-button">Buy now!</a>
       </div>
   `,
-  styleUrls: ['shop-item.component.css']
+   styleUrls: ['shop-item.component.css']
 })
 export class ShopItemComponent implements OnInit {
-  @Input() item: ShopItem;
+   @Input() item: ShopItem;
 
-  ngOnInit() {
-    
-  }
+   ngOnInit() {
+
+   }
 }
